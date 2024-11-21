853f656bd0153fac6348864026df483aca8dd161
arashmodrad <arashmodaresirad@u.boisestate.edu>
2024-11-21 12:08:10 -0700

    fixed temp file cleanup and adress

--- tools/mask_bounds.py
+++ tools/mask_bounds.py
@@ -3,6 +3,7 @@ import duckdb
 import ibis
 from ibis import _
 import geopandas as gpd
+import os
 
 ibis.options.interactive = True
 
@@ -81,27 +82,39 @@ def create_general_mask(database_path: str, triangles_path: str,
     # 4. "mask_nwm_lakes_conus.shp","interior"
     # 5. "mask_water_polygon_conus.shp","interior" 
     step_1 = gpd.overlay(sch_bb, sch_b, how='difference')
+    step_1 = step_1[['geometry']]
     step_2 = gpd.GeoDataFrame(geometry=state.buffer(5))
     step_2 = step_2.dissolve(by=None)
     step_2 = gpd.overlay(step_2, state, how='difference')
     step_2 = gpd.overlay(step_2, sch_b, how='difference')
     step_2 = gpd.overlay(step_1, step_2, how='union').dissolve(by=None)
+    step_2 = step_2[['geometry']]
     if dissolve:
         step_3 = gpd.overlay(step_2, levee, how='union').dissolve(by=None)
+        step_3 = step_3[['geometry']]
         step_4 = gpd.overlay(step_3, nwm, how='union').dissolve(by=None)
+        step_4 = step_4[['geometry']]
         step_5 = gpd.overlay(step_4, water, how='union').dissolve(by=None)
+        step_5 = step_5[['geometry']]
     else:
         step_3 = gpd.overlay(step_2, levee, how='union')
+        step_3 = step_3[['geometry']]
         step_4 = gpd.overlay(step_3, nwm, how='union')
+        step_4 = step_4[['geometry']]
         step_5 = gpd.overlay(step_4, water, how='union')
+        step_5 = step_5[['geometry']]
     # Ensure crs is set
     step_5 = step_5.set_crs("EPSG:4326") 
-
-    # write to database
-    step_5.to_parquet('atlgulf/mask.parquet', index=False)
+    
+    # Write to database
+    directory = 'temp'
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+    temp_file = os.path.join(directory, 'mask.parquet')
+    step_5.to_parquet(temp_file, index=False)
     mask_conn.raw_sql(f"""
                         CREATE OR REPLACE TABLE step_5 AS 
-                        SELECT * FROM 'atlgulf/mask.parquet'
+                        SELECT * FROM '{temp_file}'
                         """)
     mask_conn.raw_sql(f"""
                         CREATE OR REPLACE TABLE triangles AS 
@@ -112,7 +125,7 @@ def create_general_mask(database_path: str, triangles_path: str,
     mask_conn.raw_sql("""
                         CREATE OR REPLACE TABLE triangles_masked AS
                         SELECT 
-                            t.* -- Keep all columns from the triangles table
+                            t.* -- Keep pg_id from the table
                         FROM 
                             triangles AS t, step_5 AS s
                         WHERE 
@@ -120,4 +133,8 @@ def create_general_mask(database_path: str, triangles_path: str,
                             AND NOT ST_Within(t.geometry, s.geometry) -- Exclude those completely within step_5
                             OR NOT ST_Intersects(t.geometry, s.geometry); -- Keep triangles that do not intersect at all
                         """)
+    # Cleanup
+    os.remove(temp_file)
+    os.rmdir(directory)
+
     return
